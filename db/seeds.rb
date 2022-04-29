dfd949a2c414144b82604a3d31f1aa485ac0cf4c
Jose Gustavo <dsn4.limport@gmail.com>
2022-04-29 15:14:50 -0300

    feat: enable client credentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Doorkeeper::Application.count.zero?
+  Doorkeeper::Application.create!(name: "App 1", redirect_uri: "", scopes: "")
+  Doorkeeper::Application.create!(name: "App 2", redirect_uri: "", scopes: "")
+end
